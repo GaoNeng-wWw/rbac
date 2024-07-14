3b7ec9aa5306457c453724a5e272d4347dc9e5ca
GaoNeng-wWw <gaonenggg@outlook.com>
2024-07-14 11:09:55 +0800

    wip feat: 异常处理
    user服务异常处理

---
+++ tiny-pro/tinypro-user-service/src/main/java/com/huaweicloud/userservice/ErrorHandler.java
@@ -0,0 +1,32 @@
+package com.huaweicloud.userservice;
+
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.net.http.HttpClient;
+
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private HttpServletResponse res;
+    @ExceptionHandler(HttpClientErrorException.class)
+    public String handleException(HttpClientErrorException e) {
+        this.res.setStatus(e.getStatusCode().value());
+        JSONObject json = JSONUtil.createObj();
+        json.set("statusCode", e.getStatusCode().value());
+        json.set("message", e.getStatusText());
+        return json.toJSONString(0);
+    }
+}
