ffa716089d8bed7b6b879435560288233312c38f
GaoNeng-wWw <gaonenggg@outlook.com>
2024-07-26 09:19:10 +0800

    feat: global registe breadcrumb component

--- tinypro-frontend/src/main.ts
+++ tinypro-frontend/src/main.ts
@@ -11,18 +11,16 @@ import 'echarts4/map/js/china.js';
 import chinaMap from './assets/chaina.json';
 // import * as echarts4 from 'echarts4';
 import {registerMap} from 'echarts';
-import { Breadcrumb } from "@opentiny/vue";
+import Breadcrumb from "@/components/breadcrumb/index.vue";
 
 registerMap('china', chinaMap as any);
 
 Vue.use(PiniaVuePlugin);
+Vue.component('Breadcrumb',Breadcrumb)
 
 new Vue({
   router,
   pinia,
   render: (h) => h(App),
   i18n: initI18({locale: 'zhCN'}),
-  components: {
-    Breadcrumb: Breadcrumb
-  }
 }).$mount("#app");
